fcab7bbcdd869645ee5c8886494ecd07e1e8a3a6
Bojan Keca <bojakeca@amazon.com>
2017-09-30 00:15:00 -0700

    added all() and some minor changes

--- tests/tests.py
+++ tests/tests.py
@@ -13,6 +13,9 @@ import sys
 import tempfile
 import unittest
 
+import boto3
+import moto
+
 from nodb import NoDB
 
 def random_string(length):
@@ -48,7 +51,7 @@ class TestNoDB(unittest.TestCase):
 
     def test_nodb(self):
         self.assertTrue(True)
-        nodb = NoDB()
+        nodb = NoDB('dummy')
         nodb.index = "Name"
 
         jeff = {"Name": "Jeff", "age": 19}
@@ -60,9 +63,25 @@ class TestNoDB(unittest.TestCase):
         serialized = nodb._serialize(jeff)
         nodb._deserialize(serialized)
 
+    @moto.mock_s3
+    def test_nodb_save_load(self):
+        self.assertTrue(True)
+        # create dummy bucket and store some objects
+        bucket_name = 'dummy_bucket'
+        boto3.resource('s3').Bucket(bucket_name).create()
+
+        nodb = NoDB(bucket_name)
+        nodb.index = "Name"
+
+        jeff = {"Name": "Jeff", "age": 19}
+
+        nodb.save(jeff)
+        possible_jeff = nodb.load('Jeff')
+        self.assertEqual(possible_jeff, jeff)
+
     def test_nodb_cache(self):
         self.assertTrue(True)
-        nodb = NoDB()
+        nodb = NoDB('dummy')
         nodb.index = "Name"
         nodb.cache = True
 
@@ -89,6 +108,22 @@ class TestNoDB(unittest.TestCase):
         self.assertEqual(loaded, jeff)
 
         bcp = nodb._get_base_cache_path()
+
+    @moto.mock_s3()
+    def test_nodb_all(self):
+        # create dummy bucket and store some objects
+        bucket_name = 'dummy_bucket'
+        boto3.resource('s3').Bucket(bucket_name).create()
+
+        nodb = NoDB(bucket_name)
+        nodb.index = "Name"
+
+        nodb.save({"Name": "John", "age": 19})
+        nodb.save({"Name": "Jane", "age": 20})
+
+        all_objects = nodb.all()
+        self.assertListEqual([{"Name": "John", "age": 19}, {"Name": "Jane", "age": 20}], all_objects)
+
 
 if __name__ == '__main__':
     unittest.main()
